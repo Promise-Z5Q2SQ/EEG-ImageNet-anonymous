4c3113d99313c8fecc3e8b96a7f3c11272cca17d
Shuqi Zhu <zsq19991106@gmail.com>
2024-10-02 00:07:35 +0800

    init

---
+++ scipt/classification.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PYTHON_SCRIPT="../src/object_classification.py"
+
+DATA_DIR="../data/"
+G_OPTION="all"
+M_OPTION="eegnet"
+B_OPTION=80
+S_OPTION=0
+P_OPTION="eegnet_s${S_OPTION}_1x_0.pth"
+O_OPTION="../output/"
+
+#python $PYTHON_SCRIPT -d $DATA_DIR -g $G_OPTION -m $M_OPTION -b $B_OPTION -p $P_OPTION -s $S_OPTION -o $O_OPTION
+#python $PYTHON_SCRIPT -d $DATA_DIR -g $G_OPTION -m $M_OPTION -b $B_OPTION -s $S_OPTION -o $O_OPTION
+
+for i in {0..15}
+do
+    P_OPTION1="eegnet_s${i}_1x_1.pth"
+#    python $PYTHON_SCRIPT -d $DATA_DIR -g $G_OPTION -m $M_OPTION -b $B_OPTION -s $i -o $O_OPTION
+    python $PYTHON_SCRIPT -d $DATA_DIR -g $G_OPTION -m $M_OPTION -b $B_OPTION -p $P_OPTION1 -s $i -o $O_OPTION
+done
